cdf6c1a49dbee46568aa23d7ccb78d8e0ee66404
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-06-28 14:23:48 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -9,7 +9,7 @@ else
 fi
 
 
-if [ -d "praveen" ]
+if [ -d "env" ]
 then
     echo "Python virtual environment exists."
 else
@@ -22,7 +22,7 @@ echo $PWD
 echo -e "\n\n\n"
 
 echo "Activating the virtual environment"
-source praveen/bin/activate
+source env/bin/activate
 echo -e "\n\n\n"
 
 
