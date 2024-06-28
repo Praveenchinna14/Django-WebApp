5e4577030d88ef86c1d71231574bdba5bc59f093
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-06-28 15:29:26 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
-    print(f"django version: {django.get_version()}")
-    print(f"Command: {sys.argv}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
